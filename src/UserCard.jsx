6349f55ca8a3fb30f12ccde07b53e4f1474d1a7f
gagahp <111202416096@mhs.dinus.ac.id>
2025-11-04 09:25:57 +0700

    UTS A11.2024.16096

--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,33 +1,47 @@
 import React, { useState } from 'react'
 
 function UserCard(props) {
-    const { name, email, street, city, ...rest } = props;
+    // const { name, email, street, city, ...rest } = props;
+    const { title, body, ...rest} = props;
     const [clicked, setClicked] = useState(false);
 
-    console.log(Object.entries(rest));
+    console.log(rest);
 
     return (
-        <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition-shadow">
-        <h2 className="text-xl font-semibold text-gray-800 mb-2">{name}</h2>
-        <p className="text-gray-600">
-            <span className="font-medium">Email:</span> {email}
-        </p>
-        <p className="text-gray-600">
-            <span className="font-medium">Address:</span> {street}, {city}
-        </p>
+        <div class="group">
+            <div className="
+            bg-white p-6 rounded-lg shadow hover:shadow-md hover:bg-red-50 hover:border hover:border-gray-500 transition-shadow text-center hover:scale-105 transition-transform duration-300
+            flex flex-col justify-between h-full
+            ">
+            {/* <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition-shadow text-center "> */}
+                <div className="">
+                    <h1 className="text-xl font-semibold text-gray-800 mb-2 capitalize">{title}</h1>
+                    <p className="text-gray-600 group-hover:text-red">{body}</p>
+                </div>
 
-        {/* Menampilkan data tambahan dari rest */}
-        {Object.entries(rest).map(([key, value]) => (
-            <p key={key} className="text-gray-600">
-                <span className="font-medium capitalize">{key}:</span> {value}
-            </p>
-        ))}
+                {/* <h2 className="text-xl font-semibold text-gray-800 mb-2">{name}</h2>
+                <p className="text-gray-600">
+                    <span className="font-medium">Email:</span> {email}
+                </p>
+                <p className="text-gray-600">
+                    <span className="font-medium">Address:</span> {street}, {city}
+                </p> */}
 
-        <button 
-            className={`${clicked ? "bg-special-green" : "bg-gray-01"} text-white p-2 rounded-md`}
-            onClick={() => setClicked(true)}>
-                {clicked ? "Tombol sudah diklik" : "Silakan klik"}
-        </button>
+                {/* Menampilkan data tambahan dari rest */}
+                {/* {Object.entries(rest).map(([key, value]) => (
+                    <p key={key} className="text-gray-600">
+                        <span className="font-medium capitalize">{key}:</span> {value}
+                    </p>
+                ))} */}
+
+                <div className="flex items-center justify-between">
+                <button 
+                    className={`${clicked ? "group-hover:bg-special-red2 bg-special-red" : "group-hover:bg-gray-02 bg-gray-01"} text-white p-2 rounded-md w-full`}
+                    onClick={() => setClicked(true)}>
+                        {clicked ? "Tombol sudah diklik" : "Silakan Klik"}
+                </button>
+                </div>
+            </div>
         </div>
     );
 }
